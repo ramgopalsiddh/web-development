a26b2a5021a4c9a928af4024f7740249da62de68
ramgopalsiddh <ramgopalsiddh.000@gmail.com>
2022-10-12 20:12:16 +0530

    loading state change in js

--- word_game/word_game.js
+++ word_game/word_game.js
@@ -6,6 +6,12 @@ async function init(){
     let currentGuess = '';
     let currentRow = 0;
 
+    const res = await fetch("https://words.dev-apis.com/word-of-the-day");
+    const resObj = await res.json();
+    const word = resObj.word.toUpperCase();
+    setLoading(false);
+
+
     function addLetter (letter) {
         if (currentGuess.length < ANSWER_LENGTH) {
             //add letter at end 
@@ -59,6 +65,10 @@ async function init(){
 
 function isLetter(letter) {
     return /^[a-zA-Z]$/.test(letter);
+  }
+
+  function setLoading(isLoading) {
+    loadingDiv.classList.toggle('show', isLoading);
   }
   
 init();
